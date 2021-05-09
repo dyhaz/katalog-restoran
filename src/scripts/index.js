a040b5cfca09cf8d8ad00e3576fb24acf6e0b2b2
dyhaz <muhammad@visionet.co.id>
2021-05-10 06:51:34 +0700

    feat: add dynamic content rendering

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,31 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
 console.log('Hello Coders! :)');
+let xmlhttp = new XMLHttpRequest();
+let url = "DATA.json";
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        let res = JSON.parse(this.responseText);
+        processData(res);
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function processData(res) {
+    let arr = res.restaurants;
+    for (let i = 0 ; i < arr.length ; i ++) {
+        let element = `
+            <div class="card-item">
+                <img width="320" height="213" src="${arr[i].pictureId}" alt="${arr[i].name}">
+                <div>
+                    <p class="rating">Rating: ${arr[i].rating}</p>
+                    <h4>${arr[i].name}</h4>
+                    <p>${arr[i].description}</p>
+                </div>
+            </div>
+        `
+        document.getElementsByClassName("card")[0].insertAdjacentHTML('beforeend', element);
+    }
+}
