0e11e61b5c712297411cefb7d6186f397270f136
dyhaz <muhammad-11@fst.unair.ac.id>
2021-05-19 08:50:08 +0700

    feat: add icon to navigation menu

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -6,3 +6,30 @@ hamburgerButtonElement.addEventListener("click", event => {
     drawerElement.classList.toggle("open");
     event.stopPropagation();
 });
+
+document.addEventListener("click", (evt) => {
+    const navElement = document.getElementById("drawer");
+    let targetElement = evt.target; // clicked element
+
+    if (drawerElement.classList.contains("open")) {
+        do {
+            if (targetElement == navElement) {
+                // This is a click inside. Do nothing, just return.
+                return;
+            }
+            // Go up the DOM
+            targetElement = targetElement.parentNode;
+        } while (targetElement);
+
+        // This is a click outside.
+        drawerElement.classList.toggle("open");
+    }
+});
+
+// setInterval(() => {
+//     if (drawerElement.classList.contains("open")) {
+//         setTimeout(() => {
+//             drawerElement.classList.toggle("open");
+//         }, 2000);
+//     }
+// }, 3000);
