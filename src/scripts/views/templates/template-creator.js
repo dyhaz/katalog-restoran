db0bde37ab2c5df15515cd2f1828e5fec2cd5204
dyhaz <muhammad-11@fst.unair.ac.id>
2021-07-16 15:09:00 +0700

    fix: fix restaurant detail layout

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -2,46 +2,58 @@ import CONFIG from '../../globals/config';
 import truncateStr from '../../utils/string-utils';
 
 const createRestaurantDetailTemplate = (rest) => `
-  <h2 class="movie__title">${rest.name}</h2>
-  <img class="movie__poster" src="${CONFIG.RESTAURANT_API.BASE_IMAGE_URL.MEDIUM + rest.pictureId}" alt="${rest.name}" />
-  <div class="movie__info">
-    <h3>Information</h3>
-    <h4>Description</h4>
-    <p>${rest.description}</p>
-    <h4>Release Date</h4>
-    <p>${rest.release_date}</p>
-    <h4>Duration</h4>
-    <p>${rest.runtime} minutes</p>
-    <h4>Rating</h4>
-    <p>${rest.vote_average}</p>
-  </div>
-  <div class="movie__overview">
-    <h3>Overview</h3>
-    <p>${rest.overview}</p>
+  <div class="restaurant__content">
+    <div class="row">
+        <div class="col">
+          <img class="restaurant__poster" src="${CONFIG.RESTAURANT_API.BASE_IMAGE_URL.MEDIUM + rest.pictureId}" alt="${rest.name}" />
+        </div>
+        <div class="col">
+          <h2 class="restaurant__title">${rest.name}</h2>
+          <div class="restaurant__info">
+            <div class="star-wrapper">
+              <span class="fas fa-star s1 ${rest.rating >= 1 ? 'active' : ''}"></span>
+              <span class="fas fa-star s2 ${rest.rating >= 2 ? 'active' : ''}"></span>
+              <span class="fas fa-star s3 ${rest.rating >= 3 ? 'active' : ''}"></span>
+              <span class="fas fa-star s4 ${rest.rating >= 4 ? 'active' : ''}"></span>
+              <span class="fas fa-star s5" ${rest.rating === 5 ? 'active' : ''}></span>
+              (${`${rest.rating}/ 5`})
+            </div>
+            <p>${rest.customerReviews.length} review${rest.customerReviews.length > 1 ? 's' : ''}</p>
+            <h4>Description</h4>
+            <p>${rest.description}</p>
+            <h4>Address</h4>
+            <p>${rest.address}</p>
+          </div>
+          <div class="restaurant__overview">
+            <h3>Overview</h3>
+            <p>${rest.overview}</p>
+          </div>
+        </div>
+    </div>
   </div>
 `;
 
 const createRestaurantItemTemplate = (rest, i) => `
-            <div class="card-item">
-                <div class="content-box">
-                    <img height="213" class="img${i}" src="${CONFIG.RESTAURANT_API.BASE_IMAGE_URL.MEDIUM + rest.pictureId}" alt="${rest.name}">
-                    <div id="ribbon-container">
-                        <a href="#" id="ribbon" target="_blank">${rest.city}</a>
-                    </div>
-                </div>
-                <div>
-                    <div class="star-wrapper">
-                      <span class="fas fa-star s1 ${rest.rating >= 1 ? 'active' : ''}"></span>
-                      <span class="fas fa-star s2 ${rest.rating >= 2 ? 'active' : ''}"></span>
-                      <span class="fas fa-star s3 ${rest.rating >= 3 ? 'active' : ''}"></span>
-                      <span class="fas fa-star s4 ${rest.rating >= 4 ? 'active' : ''}"></span>
-                      <span class="fas fa-star s5" ${rest.rating === 5 ? 'active' : ''}></span>
-                      (${`${rest.rating}/ 5`})
-                    </div> 
-                    <h4><a class="title-href" href="${`/#/detail/${rest.id}`}">${rest.name}</a></h4>
-                    <p>${truncateStr(rest.description, CONFIG.MAX_CHARS)}</p>
-                </div>
-            </div>
+  <div class="card-item">
+      <div class="content-box">
+          <img height="213" class="img${i}" src="${CONFIG.RESTAURANT_API.BASE_IMAGE_URL.MEDIUM + rest.pictureId}" alt="${rest.name}">
+          <div id="ribbon-container">
+              <a href="#" id="ribbon" target="_blank">${rest.city}</a>
+          </div>
+      </div>
+      <div>
+          <div class="star-wrapper">
+            <span class="fas fa-star s1 ${rest.rating >= 1 ? 'active' : ''}"></span>
+            <span class="fas fa-star s2 ${rest.rating >= 2 ? 'active' : ''}"></span>
+            <span class="fas fa-star s3 ${rest.rating >= 3 ? 'active' : ''}"></span>
+            <span class="fas fa-star s4 ${rest.rating >= 4 ? 'active' : ''}"></span>
+            <span class="fas fa-star s5" ${rest.rating === 5 ? 'active' : ''}></span>
+            (${`${rest.rating}/ 5`})
+          </div> 
+          <h4><a class="title-href" href="${`/#/detail/${rest.id}`}">${rest.name}</a></h4>
+          <p>${truncateStr(rest.description, CONFIG.MAX_CHARS)}</p>
+      </div>
+  </div>
 `;
 
 const createMovieDetailTemplate = (movie) => `
